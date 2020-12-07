028cefddb16b445b6d5a1837c7b044043fd2778b
theus <matheus11_santana@hotmail.com>
2020-12-07 14:37:00 -0300

    Primeiro programa em java

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+
+//java é uma linguagem case sensitive, nomes de classe sempre devem começar com letra maiúscula por palavra, metodos atributos etc começam com minúscula e
+// dps maiúscula, pacotes sempre são caixa baixa!!!
+public class PrimeiroPrograma{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+}
+
+}
