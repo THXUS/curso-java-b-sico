1a106588de8b73052adb875e48ce95f53d2df17d
theus <matheus11_santana@hotmail.com>
2020-12-10 16:36:04 -0300

    adiçoes  na aula de condicionais

--- IdadeVotacao.java
+++ IdadeVotacao.java
@@ -1,4 +1,7 @@
+import java.util.Scanner; //escrever scanner sempre com S 
+
 public class IdadeVotacao {
+
     public static void main(String[] args) {
         int nasc = 2004;
         int idade = 2020 - nasc;
@@ -17,6 +20,30 @@ public class IdadeVotacao {
                 System.out.println("Voto obrigatório");
             }
         }
+        
+    Scanner input = new Scanner(System.in); //*para fazermos input na linguagem usamos isso!!!!!
+    int pernas;
+    System.out.println("Quantas pernas? ");
+    pernas = Integer.parseInt(input.nextLine());
+    switch(pernas)
+    {
+        case 1:
+        System.out.println("Saci");
+        break;
+        case 2:
+        System.out.println("Bípede");
+        break;
+        case 4:
+        System.out.println("Quadrupede");
+        break;
+        case 6:
+        System.out.println("Aranha");
+        break;
+        default:
+        System.out.println("ET");
+        break;
+    }
+        
     }
     
 }
